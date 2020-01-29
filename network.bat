7cdf7a50e2b52724ecc38fa022b856db3f05ac71
Djalin <Djalin@users.noreply.github.com>
2020-01-29 12:09:14 +0200

    Add files via upload

---
+++ network.bat
@@ -0,0 +1,15 @@
+rem save old configuration
+ipconfig /all >ip_%COMPUTERNAME%_%USERNAME%_log.txt
+rem clean dns cache
+ipconfig /flushdns
+rem refresh network settings
+netsh winsock reset
+rem clean hosts file
+echo 127.0.0.1	localhost >%windir%\System32\drivers\etc\hosts
+rem save new configuration
+ipconfig /all >>ip_%COMPUTERNAME%_%USERNAME%_log.txt
+rem test ping
+ping google.com >>ip_%COMPUTERNAME%_%USERNAME%_log.txt
+ping 8.8.8.8 >>ip_%COMPUTERNAME%_%USERNAME%_log.txt
+rem test tracert
+tracert 8.8.8.8 >>ip_%COMPUTERNAME%_%USERNAME%_log.txt
