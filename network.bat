5a67808ced176b25009ba7aea5058bca2d1caee0
Djalin <Djalin@users.noreply.github.com>
2020-01-30 11:19:14 +0200

    Update network.bat

--- network.bat
+++ network.bat
@@ -1,5 +1,6 @@
 rem save old configuration
-ipconfig /all >ip_%COMPUTERNAME%_%USERNAME%_log.txt
+echo old configuration >ip_%COMPUTERNAME%_%USERNAME%_log.txt
+ipconfig /all >>ip_%COMPUTERNAME%_%USERNAME%_log.txt
 rem clean dns cache
 ipconfig /flushdns
 rem refresh network settings
@@ -7,6 +8,7 @@ netsh winsock reset
 rem clean hosts file
 echo 127.0.0.1	localhost >%windir%\System32\drivers\etc\hosts
 rem save new configuration
+echo new configuration >>ip_%COMPUTERNAME%_%USERNAME%_log.txt
 ipconfig /all >>ip_%COMPUTERNAME%_%USERNAME%_log.txt
 rem test ping
 ping google.com >>ip_%COMPUTERNAME%_%USERNAME%_log.txt
