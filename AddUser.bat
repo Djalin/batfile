312bcfd62ebf47950aa8e8338fd8fb85c9402257
Djalin <Djalin@users.noreply.github.com>
2020-02-18 11:32:58 +0200

    Update AddUser.bat

--- AddUser.bat
+++ AddUser.bat
@@ -2,7 +2,10 @@ rem add users
 rem run as administrator, replace the password as needed, replace the Administrators as needed
 net user user /add
 net user admin password /add
+rem locale english
 net localgroup Administrators admin /add
+rem locale RU
+rem net localgroup Администратор admin /add
 net localgroup Users admin /delete
 net accounts /maxpwage:unlimited
 EXIT
