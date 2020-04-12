a1ad4474801f2591f80853120b309407b9a6f4f2
Djalin <Djalin@users.noreply.github.com>
2020-04-12 20:48:24 +0300

    Update AddUser.bat

--- AddUser.bat
+++ AddUser.bat
@@ -1,4 +1,4 @@
-rem add users
+rem add users readme comand to https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-r2-and-2012/cc771865(v=ws.11)
 rem run as administrator, replace the password as needed, replace the Administrators as needed
 net user user /add
 net user admin password /add
